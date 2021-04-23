39fb447ca3c85dd468eaa72106ba9377524092ee
SujayTank <61505947+SujayTank@users.noreply.github.com>
2021-04-23 23:31:55 +0530

    small updates

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,11 @@
+import { TopupPartnerComponent } from './dashboard/partner/topup-partner/topup-partner.component';
+import { RecentPartnerComponent } from './dashboard/partner/recent-partner/recent-partner.component';
+import { FindPartnerComponent } from './dashboard/partner/find-partner/find-partner.component';
+import { ManagePartnerComponent } from './dashboard/partner/manage-partner/manage-partner.component';
+import { TopupEmployeeComponent } from './dashboard/employee/topup-employee/topup-employee.component';
+import { BlackedListedEmployeeComponent } from './dashboard/employee/blacked-listed-employee/blacked-listed-employee.component';
+import { FindEmployeeComponent } from './dashboard/employee/find-employee/find-employee.component';
+import { ManageEmployeeComponent } from './dashboard/employee/manage-employee/manage-employee.component';
 
 import { TopupMemberComponent } from './dashboard/customer/topup-member/topup-member.component';
 import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
@@ -12,6 +20,8 @@ import { ManageMemberComponent } from './dashboard/customer/manage-member/manage
 import { FindMemberComponent } from './dashboard/customer/find-member/find-member.component';
 import { BlackListComponent } from './dashboard/customer/black-list/black-list.component';
 import { RecentMemberComponent } from './dashboard/customer/recent-member/recent-member.component';
+import { RecentEmployeeComponent } from './dashboard/employee/recent-employee/recent-employee.component';
+import { BlackListedPartnerComponent } from './dashboard/partner/black-listed-partner/black-listed-partner.component';
 
 const routes: Routes = [
 { path: '', component: LoginComponent, pathMatch: 'full' },
@@ -20,11 +30,24 @@ const routes: Routes = [
 { path: 'aboutUs', component: HomeComponent, pathMatch: 'full' },
 { path: 'registration', component: RegisterComponent, pathMatch: 'full' },
 { path: 'dashboard', component: DashboardComponent,canActivate: [AuthguardGuard], },
-{ path: 'dashboard/manage-member', component: ManageMemberComponent },
-{ path: 'dashboard/find-member', component: FindMemberComponent },
-{ path: 'dashboard/black-listed', component: BlackListComponent },
-{ path: 'dashboard/recent-member', component: RecentMemberComponent },
-{ path: 'dashboard/topup-member', component: TopupMemberComponent },
+
+{ path: 'dashboard/customer/manage-member', component: ManageMemberComponent },
+{ path: 'dashboard/customer/find-member', component: FindMemberComponent },
+{ path: 'dashboard/customer/black-listed', component: BlackListComponent },
+{ path: 'dashboard/customer/recent-member', component: RecentMemberComponent },
+{ path: 'dashboard/customer/topup-member', component: TopupMemberComponent },
+
+{ path: 'dashboard/employee/manage-member', component: ManageEmployeeComponent },
+{ path: 'dashboard/employee/find-member', component: FindEmployeeComponent },
+{ path: 'dashboard/employee/black-listed', component: BlackedListedEmployeeComponent },
+{ path: 'dashboard/employee/recent-member', component: RecentEmployeeComponent },
+{ path: 'dashboard/employee/topup-member', component: TopupEmployeeComponent },
+
+{ path: 'dashboard/partner/manage-member', component: ManagePartnerComponent },
+{ path: 'dashboard/partner/find-member', component: FindPartnerComponent },
+{ path: 'dashboard/partner/black-listed', component: BlackListedPartnerComponent },
+{ path: 'dashboard/partner/recent-member', component: RecentPartnerComponent },
+{ path: 'dashboard/partner/topup-member', component: TopupPartnerComponent },
 // { path: '**', component: PageNotFoundComponent },
 
 ]
