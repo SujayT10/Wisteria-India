7aa3880a0bc4040f0f55f6eba70e697099a8b885
SujayTank <61505947+SujayTank@users.noreply.github.com>
2021-05-15 11:29:23 +0530

    small updation in partner componet

--- src/app/partner-dashboard/pPartner/manage-ppartner/manage-ppartner.component.ts
+++ src/app/partner-dashboard/pPartner/manage-ppartner/manage-ppartner.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Partner } from 'src/app/classes/partner';
 import { PartnerService } from 'src/app/services/partner.service';
+import { first } from 'rxjs/operators';
 
 @Component({
   selector: 'app-manage-ppartner',
@@ -8,11 +10,23 @@ import { PartnerService } from 'src/app/services/partner.service';
 })
 export class ManagePpartnerComponent implements OnInit {
   partner_id: string;
+  partner: Partner[];
+  totalLength: any;
+  page: number = 1;
 
   constructor(private partnerService: PartnerService) { }
 
   ngOnInit(): void {
     this.partner_id = this.partnerService.getToken();
+    this.postId(this.partner_id);
+  }
+
+  public postId(partner_id: any){
+    this.partnerService.postDataToDB(partner_id)
+        .pipe(first())
+        .subscribe((data: Partner[]) =>{
+          this.partner= data;
+        })
   }
 
 }
