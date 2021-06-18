33e53959cee6dae4a6b161dbd6a7aa4ea3a4d3d4
SujayTank <61505947+SujayTank@users.noreply.github.com>
2021-06-18 13:21:38 +0530

    final form edited

--- src/app/components/talent-hunt-register/talent-hunt-register.component.ts
+++ src/app/components/talent-hunt-register/talent-hunt-register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PartnerService } from 'src/app/services/partner.service';
 
 @Component({
   selector: 'app-talent-hunt-register',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TalentHuntRegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private partnerService: PartnerService) { }
 
   ngOnInit(): void {
+
+    this.partnerService.show();
+
     $(document).ready(function(){
       //group add limit
       var maxGroup = 5;
